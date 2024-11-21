83bc9d6c6a4cfbe2befbf91c20d440d4f9e8866d
Endy Muhardin <endy.muhardin@gmail.com>
2024-11-21 14:20:43 +0700

    setup database

---
+++ flyway/migrations/V2024112101__Skema_Awal.sql
@@ -0,0 +1,6 @@
+create table customer (
+    id varchar(36), 
+    name varchar(200) not null,
+    email varchar(100) not null,
+    primary key (id)
+);
---
+++ test/sql/sample-customer.sql
@@ -0,0 +1,6 @@
+insert into customer (id, name, email) values 
+('c001', 'Customer 001', 'customer.001@yopmail.com'),
+('c002', 'Customer 002', 'customer.002@yopmail.com'),
+('c003', 'Customer 003', 'customer.003@yopmail.com'),
+('c004', 'Customer 004', 'customer.004@yopmail.com'),
+('c005', 'Customer 005', 'customer.005@yopmail.com');

af804bc28cfe5716b29655d4119cd5040a44a9fa
Endy Muhardin <endy.muhardin@gmail.com>
2024-11-21 15:42:53 +0700

    remove warning

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://ewallet:ewallet123@localhost:54321/ewalletdb"
